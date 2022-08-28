333fa107f69e1443b69fc64e67846a7d6ed618a9
Yangdi Li <yangdi.li@sennder.com>
2022-08-27 20:36:18 -0500

    progress in table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Sidebar></Sidebar>
-    <v-main style="padding: 0;">
+    <v-main style="padding: 0;" class="ml-16">
       <router-view />
     </v-main>
   </v-app>
@@ -33,10 +33,10 @@ export default Vue.extend({
 // }
 
 * {
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
   font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/ScheduleManagerView.vue
+++ src/views/ScheduleManagerView.vue
@@ -1,19 +1,26 @@
 <template>
-    <v-container grid-list-xs>
-        <div class="container">
-            <v-simple-table fixed-header>
-                <template v-slot:default>
-                    <thead>
-                        <tr>
-                            <th v-for="(header, index) in tableHeaders" :key="index">
-                                {{ header }}
-                            </th>
-                        </tr>
-                    </thead>
-                    <draggable :list="myArray" tag="tbody" @change="log">
-                        <tr v-for="(row, index) in myArray" :key="index">
-                            <td v-for="(column, index) in row" :key="index">{{column}}</td>
-                            <!-- <td> {{ row.time }}</td>
+    <v-container grid-list-xs fluid>
+        <v-simple-table fixed-header>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th v-for="(header, index) in tableHeaders" :key="index" :class="[
+                        header.name == 'time' ? 'sm-col' : '',
+                        header.name == 'estimate' ? 'md-col' : '',
+                        header.name == 'game' ? 'lg-col' : '']">
+                            <!-- <th v-for="(header, index) in tableHeaders" :key="index" class="header-container"> -->
+                            {{ header.text }}
+                        </th>
+                    </tr>
+                </thead>
+                <draggable :list="myArray" tag="tbody" @change="log">
+                    <tr v-for="(row, rowIndex) in myArray" :key="rowIndex">
+                        <td v-for="(column, index) in row" :key="index">
+                            <v-text-field full-width v-model="row[index]" :name="`${rowIndex}${index}`" dense
+                                @keyup.enter="logRow(row)">
+                            </v-text-field>
+                        </td>
+                        <!-- <td> {{ row.time }}</td>
                             <td> {{ row.estimate }}</td>
                             <td> {{ row.game }}</td>
                             <td> {{ row.category }}</td>
@@ -23,54 +30,21 @@
                             <td> {{ row.comsTwitch }}</td>
                             <td> {{ row.gameTwitch }}</td>
                             <td> {{ row.runnerTwitch }}</td> -->
-                        </tr>
-                    </draggable>
-                </template>
-            </v-simple-table>
-            <!-- <v-data-table ref="myTable" v-model="selected" :headers="tableHeaders" :items="myArray" item-key="name"
-                class="elevation-1">
-                <template #body="props">
-                    <draggable :list="myArray" tag="tbody" :move="onMoveCallback" :clone="onCloneCallback"
-                        @end="onDropCallback" @change="log">
-                        <data-table-row-handler v-for="(item, index) in props.items" :key="index" :item="item"
-                            :headers="tableHeaders" :item-class="getClass(item)">
-
-                        </data-table-row-handler>
-                    </draggable>
-                </template>
-            </v-data-table> -->
-            <!-- <table>
-                <caption>SRE9</caption>
-                <thead>
-                    <tr>
-                        <th v-for="(header, index) in tableHeaders" :key="index">
-                            {{ header }}
-                        </th>
-                    </tr>
-                </thead>
-                <draggable :list="myArray" tag="tbody" @change="log">
-                    <tr v-for="(row, index) in myArray" :key="index">
-                        <td> {{ row.time }}</td>
-                        <td> {{ row.estimate }}</td>
-                        <td> {{ row.game }}</td>
-                        <td> {{ row.category }}</td>
-                        <td> {{ row.runner }}</td>
-                        <td> {{ row.platform }}</td>
-                        <td> {{ row.coms }}</td>
-                        <td> {{ row.comsTwitch }}</td>
-                        <td> {{ row.gameTwitch }}</td>
-                        <td> {{ row.runnerTwitch }}</td>
                     </tr>
                 </draggable>
-            </table> -->
-        </div>
+            </template>
+        </v-simple-table>
     </v-container>
 
-    <!-- <template #[`item.lock`]="{ item }">
-                                <v-icon @click="item.locked = item.locked ? false : true">{{
-                                        item.locked ? "mdi-pin-outline" : "mdi-pin-off-outline"
-                                }}</v-icon>
-                            </template> -->
+    <!-- <v-menu offset-y min-width=" 400px" :close-on-content-click="false">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <span v-bind="attrs" v-on="on" @click="getField(row, column)">{{ column
+                                    }}</span>
+                                </template>
+                                <v-card>
+                                    
+                                </v-card>
+                            </v-menu> -->
 </template>
 
 <script>
@@ -82,24 +56,40 @@ import API from "../api/index";
 
 export default {
     // eslint-disable-next-line
-    components: { Sidebar, draggable},
+    components: { Sidebar, draggable },
     data() {
         return {
-            tableHeaders: ['Hora', 'Estimado', 'Juego', 'Categoría', 'Runner/s', 'Plataforma', 'Comentaristas', 'comsTwitch', 'gameTwitch', 'runnerTwitch'],
+            tableHeaders: [
+                { text: 'Hora', name: 'time' },
+                { text: 'Estimado', name: 'estimate' },
+                { text: 'Juego', name: 'game' },
+                { text: 'Categoría', name: 'category' },
+                { text: 'Runner/s', name: 'runner' },
+                { text: 'Plataforma', name: 'platform' },
+                { text: 'Comentaristas', name: 'coms' },
+                { text: 'comsTwitch', name: 'comsTwitch' },
+                { text: 'gameTwitch', name: 'gameTwitch' },
+                { text: 'runnerTwitch', name: 'runnerTwitch' },
+                { text: 'setupTime', name: 'setup' }],
 
             myArray: [
-                { time: '10:00 PM', estimate: '05:30:00', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Chemigoku', platform: 'ps1', coms: 'chemigoku', comsTwitch: 'chemigoku2', gameTwitch: 'Dark Souls', runnerTwitch: 'chemigoku2' },
-                { time: '10:00 PM', estimate: '00:30:10', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Rhino', platform: 'ps1', coms: 'rhino', comsTwitch: 'rhino', gameTwitch: 'Dark Souls', runnerTwitch: 'rhino' },
-                { time: '10:00 PM', estimate: '00:45:00', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Hado', platform: 'ps1', coms: 'hado', comsTwitch: 'hado', gameTwitch: 'Dark Souls', runnerTwitch: 'hado' },
+                { time: '10:00 PM', estimate: '05:30:00', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Chemigoku', platform: 'ps1', coms: 'chemigoku', comsTwitch: 'chemigoku2', gameTwitch: 'Dark Souls', runnerTwitch: 'chemigoku2', setup: "0h10m00s" },
+                { time: '10:00 PM', estimate: '00:30:10', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Rhino', platform: 'ps1', coms: 'rhino', comsTwitch: 'rhino', gameTwitch: 'Dark Souls', runnerTwitch: 'rhino', setup: "0h10m00s" },
+                { time: '10:00 PM', estimate: '00:45:00', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Hado', platform: 'ps1', coms: 'hado', comsTwitch: 'hado', gameTwitch: 'Dark Souls', runnerTwitch: 'hado', setup: "0h10m00s" },
             ],
             allowDrag: true,
             selected: [],
+            showMenus: [],
+            dialogInput: ""
         };
     },
     methods: {
         log() {
             console.log(this.myArray[0].runner, this.myArray[1].runner, this.myArray[2].runner)
         },
+        logRow(row) {
+            console.log(row)
+        }
     },
     mounted: function () {
         // API.getCommands().then((response) => {
@@ -111,48 +101,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-table {
-    // margin-bottom: 12px;
-    // min-width: 300px;
-    // width: 100%;
-    // height: fit-content;
-    // overflow-y: scroll;
-    // border-collapse: collapse;
-    // // border: 1px solid #000;
+.theme--dark.v-data-table {
+    .v-data-table__wrapper {
+        table {
+            // table-layout: fixed;
+            // width: 100%;
+            // min-width: max-content;
 
-    thead {
-        // background-color: var(--dark-var);
-        // font-size: 0.875rem;
-        // text-transform: uppercase;
-        // text-align: left;
+            thead {
+                tr {
+                    th {
+                        &.lg-col {
+                            width: 370px;
+                        }
+                    }
+                }
+            }
 
-        // tr {
-        //     margin-bottom: 1rem;
-        // }
+            tbody {
+                tr {
 
-        // tr th {
-        //     // color: var(--light);
-        //     color: #185875 !important;
-        // }
+                    // max-height: 50px !important;
+                    // min-height: fit-content;
+                    // overflow: hidden;
+                    td {
+                        // overflow: hidden;
+                        // text-overflow: ellipsis;
+                    }
+                }
+            }
+        }
     }
 
-    // tbody {
-    //     margin-top: 1rem;
+    // min-width: 76px;
+    // &.sm-col {
+    //     max-width: 76px;
+    //     width: 76px;
+    //     color: red;
     // }
 
-    // tr {
-    //     border-bottom: 1px solid black;
-
-    //     td {
-    //         // padding: 3px 5px;
-    //         vertical-align: middle;
-    //         // border: 1px solid #000;
-    //         padding: 10px;
-
-    //         @media (min-width: 800px) {
-    //             display: table-cell;
-    //         }
-    //     }
+    // &.md-col {
+    //     max-width: 100px;
     // }
+
+    // &.lg-col {
+    //     max-width: 180px;
+    // }
+
 }
 </style>
