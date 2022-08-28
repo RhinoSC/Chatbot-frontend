e4f3855eb0a38a58b1ef2b6f291a1155c6fbcd9d
Yangdi Li <yangdi.li@sennder.com>
2022-08-27 21:01:12 -0500

    draggeable disable function

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,5 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: { dark: true },
+    theme: {
+        themes: {
+            light: {
+                primary: '#607d8b',
+                secondary: '#3f51b5',
+                accent: '#03a9f4',
+                error: '#f44336',
+                warning: '#ff5722',
+                info: '#ffc107',
+                success: '#009688'
+            }
+        }
+    },
 });
--- src/views/ScheduleManagerView.vue
+++ src/views/ScheduleManagerView.vue
@@ -4,20 +4,20 @@
             <template v-slot:default>
                 <thead>
                     <tr>
-                        <th v-for="(header, index) in tableHeaders" :key="index" :class="[
-                        header.name == 'time' ? 'sm-col' : '',
-                        header.name == 'estimate' ? 'md-col' : '',
-                        header.name == 'game' ? 'lg-col' : '']">
-                            <!-- <th v-for="(header, index) in tableHeaders" :key="index" class="header-container"> -->
-                            {{ header.text }}
+                        <th v-for="(header, index) in tableHeaders" :key="index"
+                            style="color: indigo; font-size: 0.9rem;" :class="[
+                            header.name == 'time' ? 'sm-col' : '',
+                            header.name == 'estimate' ? 'md-col' : '',
+                            header.name == 'game' ? 'lg-col' : '']">
+                            <span>{{ header.text }}</span>
                         </th>
                     </tr>
                 </thead>
-                <draggable :list="myArray" tag="tbody" @change="log">
+                <draggable :list="myArray" tag="tbody" @change="log" :disabled="disableDrag">
                     <tr v-for="(row, rowIndex) in myArray" :key="rowIndex">
                         <td v-for="(column, index) in row" :key="index">
                             <v-text-field full-width v-model="row[index]" :name="`${rowIndex}${index}`" dense
-                                @keyup.enter="logRow(row)">
+                                @keyup.enter="logRow(row)" @focus="disableDrag = !disableDrag" @blur="disableDragM()">
                             </v-text-field>
                         </td>
                         <!-- <td> {{ row.time }}</td>
@@ -77,10 +77,8 @@ export default {
                 { time: '10:00 PM', estimate: '00:30:10', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Rhino', platform: 'ps1', coms: 'rhino', comsTwitch: 'rhino', gameTwitch: 'Dark Souls', runnerTwitch: 'rhino', setup: "0h10m00s" },
                 { time: '10:00 PM', estimate: '00:45:00', game: 'Scott Pilgrim vs. The World: The Game - Complete Edition', category: 'any%', runner: 'Hado', platform: 'ps1', coms: 'hado', comsTwitch: 'hado', gameTwitch: 'Dark Souls', runnerTwitch: 'hado', setup: "0h10m00s" },
             ],
-            allowDrag: true,
             selected: [],
-            showMenus: [],
-            dialogInput: ""
+            disableDrag: false
         };
     },
     methods: {
@@ -89,6 +87,11 @@ export default {
         },
         logRow(row) {
             console.log(row)
+        },
+        disableDragM() {
+            console.log('asi entra', this.disableDrag, 'asi sale', !this.disableDrag)
+            this.disableDrag = !this.disableDrag
+            return
         }
     },
     mounted: function () {
@@ -101,52 +104,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.theme--dark.v-data-table {
-    .v-data-table__wrapper {
-        table {
-            // table-layout: fixed;
-            // width: 100%;
-            // min-width: max-content;
-
-            thead {
-                tr {
-                    th {
-                        &.lg-col {
-                            width: 370px;
-                        }
-                    }
-                }
-            }
-
-            tbody {
-                tr {
-
-                    // max-height: 50px !important;
-                    // min-height: fit-content;
-                    // overflow: hidden;
-                    td {
-                        // overflow: hidden;
-                        // text-overflow: ellipsis;
-                    }
-                }
-            }
-        }
+th {
+    &.lg-col {
+        width: 370px;
     }
-
-    // min-width: 76px;
-    // &.sm-col {
-    //     max-width: 76px;
-    //     width: 76px;
-    //     color: red;
-    // }
-
-    // &.md-col {
-    //     max-width: 100px;
-    // }
-
-    // &.lg-col {
-    //     max-width: 180px;
-    // }
-
 }
+
+// .theme--dark.v-data-table {
+//     .v-data-table__wrapper {
+//         table {
+//             // table-layout: fixed;
+//             // width: 100%;
+//             // min-width: max-content;
+//             thead {
+//                 tr {
+//                     th {
+//                         &.lg-col {
+//                             width: 370px;
+//                         }
+//                     }
+//                 }
+//             }
+
+//         tbody {
+//             tr {
+
+//                 // max-height: 50px !important;
+//                 // min-height: fit-content;
+//                 // overflow: hidden;
+//                 td {
+//                     // overflow: hidden;
+//                     // text-overflow: ellipsis;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// min-width: 76px;
+// &.sm-col {
+//     max-width: 76px;
+//     width: 76px;
+//     color: red;
+// }
+
+// &.md-col {
+//     max-width: 100px;
+// }
+
+// &.lg-col {
+//     max-width: 180px;
+// }
+// }
 </style>
