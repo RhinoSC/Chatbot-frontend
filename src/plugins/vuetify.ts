c98a958038f3993c8b3c53507edb9aea549fd932
Cesar Solano <csolanoc@unal.edu.co>
2022-10-22 10:57:54 -0500

    added themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,8 +5,30 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     theme: {
+        dark: true,
         themes: {
+
+            dark: {
+                // primary: '#b39ddb', // mine
+                // secondary: '#9fa8da', // mine
+                // accent: '#6f79a8', 
+
+                primary: '#BB86FC', // mdi
+                secondary: '#03DAC5', // mdi
+                accent: '#03DAC6',
+                error: '#CF6679',
+
+                // error: '#f44336',
+                // warning: '#ff5722',
+                // info: '#ffc107',
+                // success: '#009688'
+            },
             light: {
+                primary: '#6200EE',
+                secondary: '#03DAC6',
+                accent: '#3700B3',
+                error: '#B00020',
+
                 // primary: '#607d8b',
                 // secondary: '#3f51b5',
                 // accent: '#03a9f4',
--- src/views/manage/tracker/TrackerManagerView.vue
+++ src/views/manage/tracker/TrackerManagerView.vue
@@ -2,12 +2,12 @@
   <v-col align="center">
     <h1>Tracker manager</h1>
     <v-row justify="center" class="mt-5">
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/events'">Events</v-btn>
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/users'">Users</v-btn>
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/schedules'">Schedules</v-btn>
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/runs'">Runs</v-btn>
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/prizes'">Prizes</v-btn>
-      <v-btn color="info" class="mr-2" link :to="'/manage/tracker/donations'">Donations</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/events'">Events</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/users'">Users</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/schedules'">Schedules</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/runs'">Runs</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/prizes'">Prizes</v-btn>
+      <v-btn color="primary" class="mr-2" link :to="'/manage/tracker/donations'">Donations</v-btn>
       <!-- <v-btn color="info" class="mr-2" link :to="'/manage/tracker/bids'">Bids</v-btn> -->
     </v-row>
     <router-view class="mt-10"></router-view>
