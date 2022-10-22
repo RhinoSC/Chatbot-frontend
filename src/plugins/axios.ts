bb646c7902c0653f42395dd76217024ba92d18d1
Cesar Solano <csolanoc@unal.edu.co>
2022-10-22 00:04:28 -0500

    fixing https

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://' + process.env.VUE_APP_BACKEND_URL + ':' + process.env.VUE_APP_BACKEND_PORT + '/';
+const API_URL = process.env.VUE_APP_BACKEND_URL + ':' + process.env.VUE_APP_BACKEND_PORT + '/';
 
 // console.log(API_URL)
 // const API = axios.create({
